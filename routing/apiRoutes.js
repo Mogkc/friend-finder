e5e45d70d2ab6cb754c6684a1ef61cafe5aa29da
Mogkc <carson.mogk@cox.net>
2019-04-02 13:55:50 -0700

    now correctly grabs and stores form data

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -9,30 +9,35 @@ module.exports = (app) => {
 
     // Recieves information from the client
     app.post("/api/newFriend", (req, res) => {
-        data.push(req.body);
-        res.send(calculateBestMatch(req.body) );
+        let newFriend = req.body;
+        // JSON always sends as string
+        for (let i = 0; i < 10; i++) {
+            newFriend.values[i] = parseInt(newFriend.values[i]);
+        }
+        data.push(newFriend);
+        res.send(calculateBestMatch(req.body));
     });
 
     // Calculations
-    const calculateBestMatch = function(person) {
-        if(data.length === 0) {
+    const calculateBestMatch = function (person) {
+        if (data.length === 0) {
             return "You're the first person to join!";
         }
         // Use a for loop to find the best match
         let bestMatch = undefined;
-        for(let i=0; i< data.length; i++) {
+        for (let i = 0; i < data.length; i++) {
             const comparison = compare(person.values, data[i].values);
-            if(bestMatch === undefined || comparison < bestMatch.score)
-                bestMatch = { match: data[i], score: comparison};
+            if (bestMatch === undefined || comparison < bestMatch.score)
+                bestMatch = { match: data[i], score: comparison };
         }
         // Return only the name of the person that matches best
         return bestMatch.match;
     }
 
     // This relies on the data coming in and being stored being arrays of the same length.
-    const compare = function(person, other) {
+    const compare = function (person, other) {
         let differences = 0;
-        for(let i = 0; i<person.length; i++) {
+        for (let i = 0; i < person.length; i++) {
             differences += Math.abs(person[i] - other[i]);
         }
     }
